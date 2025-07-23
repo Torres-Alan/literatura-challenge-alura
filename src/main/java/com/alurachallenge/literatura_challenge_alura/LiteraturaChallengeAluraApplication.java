dd2001de2f1f990e990233514fb6affdb796500e
Torres-Alan <adtorresfuentes14@gmail.com>
2025-07-22 20:24:57 -0600

    Primer commit del proyecto Literatura-challenge

---
+++ src/main/java/com/alurachallenge/literatura_challenge_alura/LiteraturaChallengeAluraApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.literatura_challenge_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaChallengeAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaChallengeAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alurachallenge/literatura_challenge_alura/LiteraturaChallengeAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.literatura_challenge_alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaChallengeAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
